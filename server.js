8b3cce73467413922d77b023b6d07c4701f7be9f
vndsmartins <vandessonmmartins@gmail.com>
2026-03-30 22:57:46 -0300

    tamanho das imagens

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const SEU_EMAIL = 'seu_email@contato.com'; // Para o User-Agent
 // ==========================================
 // 2. MEDIDAS E PESO FIXOS (PADRÃO CORREIOS)
 // ==========================================
-const PESO_FIXO = 0.1;        // 500 gramas
+const PESO_FIXO = 0.01;        // 500 gramas
 const ALTURA_FIXA = 4;      // 10 cm
 const LARGURA_FIXA = 10;     // 15 cm
 const COMPRIMENTO_FIXO = 13; // 20 cm
